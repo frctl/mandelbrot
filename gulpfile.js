caded823c49ee85bf83f56cedb78daf12d89eb1a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-05-13 00:25:12 +0100

    Theme configuration tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const source            = require('vinyl-source-stream');
 const buffer            = require('vinyl-buffer');
 const del               = require('del');
 
-
+//
 // JS
 //
 gulp.task('js', ['clean:js'], () => compileJS());
@@ -24,9 +24,9 @@ gulp.task('clean:js', function() {
     return del(['./dist/js']);
 });
 
-
+//
 // CSS
-
+//
 gulp.task('css:skins', function() {
     const fs = require('fs');
     const skins = require('./assets/scss/skins/_skins.json');
@@ -34,9 +34,8 @@ gulp.task('css:skins', function() {
     for (let skin of skins) {
         fs.writeFile(`./assets/scss/skins/${skin.name}.scss`,
 `
-$color-header-bg: ${skin.accent};
+$color-header-background: ${skin.accent};
 $color-header-content: ${skin.complement};
-$color-header-splitter: ${skin.complement};
 $color-link: ${skin.links};
 
 @import "../theme";
@@ -46,7 +45,7 @@ $color-link: ${skin.links};
     }
 });
 
-gulp.task('css', ['css:clean', 'css:skins'], function() {
+gulp.task('css', ['css:skins'], function() {
   return gulp.src('./assets/scss/skins/*.scss')
     .pipe(stylelint({
         reporters: [{formatter: 'string', console: true}]
@@ -70,8 +69,9 @@ gulp.task('css:watch', function () {
     gulp.watch('./assets/scss/**/*.scss', ['css']);
 });
 
-
+//
 // Fonts
+//
 gulp.task('fonts', ['fonts:clean'], function() {
    gulp.src('./assets/fonts/**/*').pipe(gulp.dest('./dist/fonts'));
 });
@@ -84,8 +84,9 @@ gulp.task('fonts:watch', function () {
     gulp.watch('./assets/fonts/**/*', ['fonts']);
 });
 
-
+//
 // Images
+//
 gulp.task('img', ['img:clean'], function() {
    gulp.src('./assets/img/**/*').pipe(gulp.dest('./dist/img'));
 });
@@ -98,14 +99,16 @@ gulp.task('img:watch', function () {
     gulp.watch('./assets/img/**/*', ['img']);
 });
 
-
+//
 // Task sets
+//
 gulp.task('watch', ['css:watch', 'js:watch', 'img:watch']);
 
 gulp.task('default', ['fonts', 'css', 'js', 'img']);
 
-
+//
 // Utils
+//
 function compileJS(watch) {
 
     let bundler = browserify('./assets/js/mandelbrot.js', {
